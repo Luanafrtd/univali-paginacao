012a87eb41617a1c954993656909101dfa69b16a
Luana Furtado <programacaolufrtd@gmail.com>
2023-04-30 19:18:23 +1200

    pagination done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <header>
+    <h1>Paginate</h1>
+
+    <ul class="pagination">
+      <div>
+      <li class="page-item"><a class="page-link" href="#">Back</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">4</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">5</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">...</a></li>
+      </div>
+      <div>
+      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      </div>
+    </ul>
+
+    </header>
   );
 }
 
